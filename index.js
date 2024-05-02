89d8593bde286ec593198c746a80a2d5e917b16a
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-05-02 13:26:16 -0300

    criando card

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const botao = document.querySelector('[data-botao]');
 const container = document.querySelector('.container');
-let resultado = [];
+
 
 botao.addEventListener('click', (e)=>{
+
+    const paragrafo = document.createElement('p');
+    paragrafo.textContent = 'Parece que o cep é invalido.';
+
     e.preventDefault();
     const campoCep = document.querySelector('[data-campo-cep]').value;
     const cep = campoCep.replace('-', '');
 
-    buscarCep(cep);
+    if(cep != '' && cep.length == 8){
+        buscarCep(cep);
+    }else{
+        container.appendChild(paragrafo);
+        return;
+    }
+
 });
 
 async function buscarCep(cep){
     try{
         const api = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         const apiConvertida = await api.json();  
-        resultado = apiConvertida;
         
-        criarCard(resultado.logradouro, resultado.bairro, resultado.localidade, resultado.ddd, resultado.uf);
+        criarCard(apiConvertida.logradouro, apiConvertida.bairro, apiConvertida.localidade, apiConvertida.ddd, apiConvertida.uf);
 
     }catch(error){
         container.innerHTML += `<p>Parece que houve um erro:${error}.</p>`
@@ -24,12 +33,21 @@ async function buscarCep(cep){
 }
 
 function criarCard(rua, bairro, cidade, ddd, uf){
-
-    container.innerHTML += `
-        <p>${rua}</p>
-        <p>${bairro}</p>
-        <p>${cidade}</p>
-        <p>${ddd}</p>
-        <p>${uf}</p>
-    `
+    const div = document.createElement('div');
+    div.classList.add('resultado');
+    const ul = document.createElement('ul');
+    const botao = document.createElement('button');
+    botao.textContent = 'Fazer nova busca';
+    ul.innerHTML = `
+        <h3>Informações:</h3>
+        <li class="item-resultado">Rua: ${rua}</li>
+        <li class="item-resultado">Bairro: ${bairro}</li>
+        <li class="item-resultado">Cidade: ${cidade}</li>
+        <li class="item-resultado">Estado: ${uf}</li>
+        <li class="item-resultado">DDD: ${ddd}</li>
+        `
+    div.appendChild(ul);
+    div.appendChild(botao)
+    container.appendChild(div);
 }
+
